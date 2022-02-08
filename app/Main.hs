3c7b15db2aae7602d6af4c48cdb35974f1278204
ShrykeWindgrace <shryke.windgrace@gmail.com>
2022-02-08 19:11:26 +0100

    Draft for assertions

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,34 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Main where
+import           Options.Applicative
+import Control.Monad.IO.Class
+import Loggers
+import Control.Monad.Reader.Class
+import Control.Monad.Error.Class
+import Control.Monad.Reader (ReaderT)
+import Control.Monad.Except (ExceptT)
+import Types (Fails)
 
 main :: IO ()
 main = pure ()
+
+
+data CliOptions = CliOptions {
+        showVersion :: Bool,
+        pathToJournal :: Maybe FilePath,
+        pathToLocker :: Maybe FilePath,
+        verbose :: Bool
+    }
+
+cliOptionsParser :: Parser CliOptions
+cliOptionsParser = CliOptions <$>
+    switch (short 'v' <> long "version" <> help "Print version and exit") <*>
+    optional (strOption (short 'f' <> long "journal-file" <> help "path to JOURNAL_FILE" <> metavar "JOURNAL_FILE")) <*>
+    optional (strOption (long "locker-file" <> help "path to LOCKER_FILE" <> metavar "LOCKER_FILE")) <*>
+    switch (long "debug" <> hidden)
+
+newtype App a = App {app :: ReaderT (Loggers (ExceptT Fails IO)) (ExceptT Fails IO) a} 
+    deriving newtype (Functor, Applicative, Monad, MonadError Fails, MonadReader (Loggers (ExceptT Fails IO)), MonadIO)
+
+runApp :: App () -> Loggers IO -> IO ()
+runApp = error "TODO"
---
+++ src/Assertions.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE FlexibleContexts #-}
+module Assertions where
+import Data.List.NonEmpty (NonEmpty ((:|)))
+import qualified Hledger.Data.Types as HDT
+import Types (Locker (Locker, verb, date, acc), Verb (Close, Open), Fails (JParsing), Fails(Asserts))
+import Data.Time.Calendar (Day)
+import Control.Monad.Except (ExceptT, runExceptT)
+import qualified Data.Text.IO as Text
+import Loggers (Loggable, logDebug)
+import Control.Monad.Error.Class ( MonadError(throwError) )
+import Control.Monad.IO.Class ( MonadIO(..) )
+import System.Exit (ExitCode (ExitSuccess))
+import Data.Maybe (fromMaybe)
+import Hledger.Read.JournalReader (parseAndFinaliseJournal, journalp)
+import Hledger.Read.Common (definputopts)
+
+data Result = Ok | NotOk (NonEmpty HDT.Transaction)
+
+runAssertion :: HDT.Journal -> Locker Day -> Result
+runAssertion (HDT.jtxns -> txns) l = let
+    errings = Prelude.filter (compTxn l) txns
+    in
+        case errings of
+            [] -> Ok
+            (h:t) -> NotOk $ h :| t
+
+-- | True if candidate violates verb condition
+comparator :: Verb -> Day -> Day -> Bool
+comparator Close mark candidate = candidate > mark
+comparator Open mark candidate = candidate < mark
+
+-- | True if this txn deals with account name and violates the verb
+compTxn :: Locker Day -> HDT.Transaction -> Bool
+compTxn l t = any (compPosting l) $ fmap (HDT.tdate t,) (HDT.tpostings t)
+
+compPosting :: Locker Day -> (Day, HDT.Posting) -> Bool
+compPosting Locker{..} (d, p) = let day = fromMaybe d (HDT.pdate p) in
+    HDT.paccount p == acc && comparator verb date day
+
+recoverJournal :: FilePath -> ExceptT String IO HDT.Journal
+recoverJournal fp =
+    liftIO (Text.readFile fp) >>= parseAndFinaliseJournal journalp definputopts fp
+
+
+runner :: (Loggable t m, MonadError Fails (t m), MonadIO (t m)) => Locker Day -> FilePath -> (t m) ExitCode
+runner l fp = do
+    jrc <- liftIO $ runExceptT $ recoverJournal fp
+    case jrc of
+      Left err -> do
+          throwError (JParsing err)
+      Right journal -> do
+          let ra = runAssertion journal l
+          case ra of
+              Ok -> 
+                  ExitSuccess <$ logDebug "Ok"
+              NotOk ne -> throwError $ Asserts $ pure (l, ne)
+                  
+                  
