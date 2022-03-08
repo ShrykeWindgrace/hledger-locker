61e9b2b5e085e2b4259c770aba7658b3c38feea1
ShrykeWindgrace <shryke.windgrace@gmail.com>
2022-03-08 18:38:28 +0100

    Try to prepend git via overrideAttrs

--- default.nix
+++ default.nix
@@ -1,4 +1,8 @@
 { pkgs ? import <nixpkgs> { }, ... }:
 let p = pkgs.haskell.packages.ghc8107.callCabal2nix "hledger-locker" ./. { };
 in
-  p // {nativeBuildInputs = [pkgs.git] ++ p.nativeBuildInputs;}
+  p.overrideAttrs (
+    oldAttrs: rec {
+      nativeBuildInputs = [pkgs.git] ++ oldAttrs.nativeBuildInputs;
+    }
+  )
