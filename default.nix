fbe73ac58da8d03db1644cb28a9795633f87bfd3
ShrykeWindgrace <shryke.windgrace@gmail.com>
2022-08-29 12:16:18 +0200

    Another attempt to nixify this application
    Credits go to https://github.com/utdemir/hs-nix-template

--- default.nix
+++ default.nix
@@ -1,8 +1,50 @@
-{ pkgs ? import <nixpkgs> { }, ... }:
-let p = pkgs.haskell.packages.ghc8107.callCabal2nix "hledger-locker" ./. { };
+{ compiler ? "ghc924" }:
+
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { };
+
+  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
+
+  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
+    overrides = hself: hsuper: {
+      "hledger-locker" =
+        (hself.callCabal2nix "hledger-locker" (gitignore ./.) { }).overrideAttrs (
+          old: rec {
+            nativeBuildInputs = [ pkgs.git ] ++ old.nativeBuildInputs;
+          }
+        );
+    };
+  };
+
+  shell = myHaskellPackages.shellFor {
+    packages = p: [
+      p."hledger-locker"
+    ];
+    buildInputs = [
+      myHaskellPackages.haskell-language-server
+      pkgs.haskellPackages.cabal-install
+      #pkgs.haskellPackages.ormolu
+      pkgs.haskellPackages.stylish-haskell
+      pkgs.haskellPackages.hlint
+      pkgs.haskellPackages.hpack
+      #pkgs.niv
+      pkgs.nixpkgs-fmt
+    ];
+    withHoogle = true;
+  };
+
+  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."hledger-locker");
+
+  #docker = pkgs.dockerTools.buildImage {
+  #name = "hledger-locker";
+  #config.Cmd = [ "${exe}/bin/hledger-locker" ];
+  #};
 in
-  p.overrideAttrs (
-    oldAttrs: rec {
-      nativeBuildInputs = [pkgs.git] ++ oldAttrs.nativeBuildInputs;
-    }
-  )
+{
+  inherit shell;
+  inherit exe;
+  #inherit docker;
+  inherit myHaskellPackages;
+  "hledger-locker" = myHaskellPackages."hledger-locker";
+}
