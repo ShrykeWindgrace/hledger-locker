5ced572e1ae36dceab2b6eb625ff599b16e81b71
ShrykeWindgrace <shryke.windgrace@gmail.com>
2024-06-07 19:23:49 +0200

    Rewrite devshell in flake

--- flake.nix
+++ flake.nix
@@ -11,22 +11,25 @@
       ghc = "ghc965";
       pkgs = nixpkgs.legacyPackages.${system};
 
-      haskellPackages = pkgs.haskell.packages.${ghc};
       packageName = "hledger-locker";
+      haskellPackages = pkgs.haskell.packages.${ghc}.extend (hself: hsuper: {
+        hledger-locker = hself.callCabal2nix packageName self { };
+      });
     in
     {
       packages.${packageName} =
-        (haskellPackages.callCabal2nix packageName self rec {
-          # Dependency overrides go here
-        }).overrideAttrs (
-          old: rec {
-            nativeBuildInputs = [ pkgs.git ] ++ old.nativeBuildInputs;
-          }
-        );
+        haskellPackages.callCabal2nix packageName self { };
       packages.default = self.packages.${system}.${packageName};
-      devShells.default = pkgs.mkShell {
-        buildInputs = [ ];
+      devShells.default = haskellPackages.shellFor {
+        nativeBuildInputs = [
+          haskellPackages.stan
+          haskellPackages.stylish-haskell
+          haskellPackages.cabal-install
+          haskellPackages.weeder
+        ];
+
         inputsFrom = builtins.attrValues self.packages.${system};
+        packages = p: [ p.hledger-locker ];
       };
     });
 }
