017ec2c259731619bb5d10490be4c150cb6df324
ShrykeWindgrace <shryke.windgrace@gmail.com>
2025-06-02 12:02:17 +0200

    Refactor flake

--- flake.nix
+++ flake.nix
@@ -12,9 +12,6 @@
       pkgs = nixpkgs.legacyPackages.${system};
 
       packageName = "hledger-locker";
-      haskellPackages = pkgs.haskell.packages.${ghc}.extend (hself: hsuper: {
-        hledger-locker = hself.callCabal2nix packageName self { };
-      });
       packagePostOverrides = pkg: with pkgs.haskell.lib.compose; pkgs.lib.pipe pkg [
         disableExecutableProfiling
         disableLibraryProfiling
@@ -29,28 +26,37 @@
 
         dontCheck
       ];
+      haskellPackages = pkgs.haskell.packages.${ghc}.extend (hself: hsuper: {
+        hledger-locker = hself.callCabal2nix packageName self { };
+        stan = packagePostOverrides hsuper.stan;
+        stylish-haskell = packagePostOverrides hsuper.stylish-haskell;
+        weeder = packagePostOverrides hsuper.weeder;
+      });
     in
     {
       packages.${packageName} =
         packagePostOverrides
-        ((haskellPackages.callCabal2nix packageName self { }).overrideAttrs(finalAttrs: previousAttrs : {
-          buildPhase = ''
-            export HLOCKER_BUILD_TIME_GIT_REV=${builtins.substring 0 7 (
-              if self ? rev then self.rev else ""
-            )}
-          '' + previousAttrs.buildPhase;
-        }));
+          ((haskellPackages.callCabal2nix packageName self { }).overrideAttrs (finalAttrs: previousAttrs: {
+            buildPhase = ''
+              export HLOCKER_BUILD_TIME_GIT_REV=${builtins.substring 0 7 (
+                if self ? rev then self.rev else ""
+              )}
+            '' + previousAttrs.buildPhase;
+          }));
       packages.default = self.packages.${system}.${packageName};
+
+      #redo this part, looks like it pull everything in the scope TODO
       devShells.default = haskellPackages.shellFor {
         nativeBuildInputs = [
           haskellPackages.stan
           haskellPackages.stylish-haskell
-          haskellPackages.cabal-install
+          #haskellPackages.cabal-install
           haskellPackages.weeder
         ];
 
         inputsFrom = builtins.attrValues self.packages.${system};
         packages = p: [ p.hledger-locker ];
       };
+      formatter = pkgs.nixpkgs-fmt;
     });
 }
